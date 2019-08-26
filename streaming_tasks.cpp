096c0b13638db8ee9dbd92c4fdc324dfeea0923a
Luciano da Silva <luciano.silva@live.com.pt>
2019-08-26 19:31:44 +0100

    streaming_task hell

--- streaming_tasks.cpp
+++ streaming_tasks.cpp
@@ -24,8 +24,8 @@
 
 #include "static_ring_buffer.hpp"
 
-#define MIN_ITERATION_RANGE 1 << 14
-#define MAX_ITERATION_RANGE 1 << 24
+#define MIN_ITERATION_RANGE 1 << 12
+#define MAX_ITERATION_RANGE 1 << 18
 using namespace glm;
 
 // data and tasks 
@@ -68,459 +68,20 @@ std::vector < transformer > generate_data(std::size_t count) {
 auto test_data{ generate_data(MAX_ITERATION_RANGE) };
 
 // prototype implementation
-namespace proto_a {
-	/*
-	struct spin_mutex {
-	public:
 
-		inline bool try_lock() {
-			return !_lockless_flag.test_and_set(std::memory_order_acquire);
-		}
+namespace proto_c {
 
-		inline void lock() {
-			while (_lockless_flag.test_and_set(std::memory_order_acquire)) {}
-		}
+	struct task;
+	struct lane;
 
-		inline void unlock() {
-			_lockless_flag.clear(std::memory_order_release);
-		}
-
-	private:
-
-		std::atomic_flag _lockless_flag = ATOMIC_FLAG_INIT;
-
-	};*/
-
-	//using spin_lock = std::unique_lock < spin_mutex >;
-
-	using callable_type = void(*)(void * begin, std::size_t length);
-
-	struct task {
-	public:
-
-		void run() {
-			if (callback)
-				callback(data, data_length);
-
-			if (parent)
-				--parent->unfinished_tasks;
-		}
-
-		inline bool is_complete() const noexcept {
-			return unfinished_tasks == 0;
-		}
-
-		task () = default;
-
-		task (callable_type m_callback, task * p_parent) :
-			callback { m_callback },
-			parent {p_parent}
-		{}
-
-		callable_type		callback{};
-		std::atomic_size_t	unfinished_tasks { 0 };
-		task *				parent { nullptr };
-		void*				data{ nullptr };
-		std::size_t			data_length{ 0 };
-		std::uint8_t 		_pad_[24]{};
+	struct exec_context {
+		proto_c::lane & lane;
+		proto_c::task * task;
 	};
 
-	template < typename _t >
-	struct allocator {
-	public:
+	using task_callback = void (*)(exec_context & cxt, void *, std::size_t );
 
-		inline void clear() {
-			_index = 0;
-		}
-
-		template < typename ... _args_tv >
-		inline task* alloc(_args_tv&& ... args) {
-			if (_index == _data.size())
-				throw std::runtime_error("Allocator capacity exceeded");
-			
-			task* t = &_data[_index];
-			new (t) _t(std::forward < _args_tv >(args)...);
-
-			++_index;
-			return t;
-		}
-
-	private:
-		std::vector < _t >	_data{ 4096 };
-		std::size_t 		_index{ 0 };
-
-	};
-
-	struct executor;
-
-	struct task_lane {
-	public:
-
-		static constexpr std::size_t mask { 4096 - 1 };
-		
-		void push(task* task_inst) {
-			auto index = bottom.load (std::memory_order_acquire);
-			tasks[index & mask] = task_inst;
-
-			bottom.store(index + 1, std::memory_order_release);
-			++tasks_pushed;
-		}
-
-		/*
-		task* pop() {
-			auto b = bottom.load();
-			auto t = top.load();
-
-			if (t < b) {
-				auto* task = tasks[b - 1];
-
-				if (bottom.compare_exchange_weak(b, b - 1))
-					return task;
-			}
-
-			return nullptr;
-		}
-
-		
-		void push (task * tast_inst) {
-			auto b = bottom.load ();
-			tasks [b & mask] = tast_inst;
-
-			bottom.store(b + 1);
-			++tasks_pushed;
-		}*/
-
-		task * pop () {
-			auto b = bottom.fetch_sub(1) - 1;
-			auto t = top.load ();
-
-			if (t <= b) {
-				task * task_instance = tasks [b & mask];
-
-				if (t != b)
-					return task_instance;
-
-				auto t_plus { t + 1 };
-				if (!top.compare_exchange_strong (t, t_plus))
-					task_instance = nullptr;
-
-				bottom.store (t_plus);
-				return task_instance;
-			} else {
-				bottom.store(t);
-				return nullptr;
-			}
-		} 
-
-		task * steal () {
-			auto t = top.load ();
-			auto b = bottom.load ();
-
-			if (t < b)  {
-				auto * task_inst = tasks [t & mask];
-
-				if (top.compare_exchange_strong(t, t + 1))
-					return task_inst;
-			} 
-
-			return nullptr;
-		}
-
-		bool is_empty() const noexcept {
-			return top == bottom;
-		}
-
-		void run_lane();
-
-		std::atomic_size_t		top{ 0 };
-		std::atomic_size_t		bottom{ 0 };
-
-		executor*				exec;
-		std::thread::id			id;
-
-		allocator < task >		local_allocator;
-		std::vector < task* >	tasks{ 4096 };
-
-		std::thread				worker;
-		std::atomic_bool		running;
-
-		std::size_t				tasks_executed{ 0 };
-		std::size_t				tasks_pushed { 0 };
-	};
-
-	struct executor {
-	public:
-
-		executor(std::size_t worker_count) :
-			_lanes(worker_count + 1)
-		{}
-
-		~executor () {
-			std::cout << "executions: " << executions << std::endl;
-			stop();
-		}
-
-		void clear_alloc () {
-			for (auto& lane : _lanes)
-				lane.local_allocator.clear();
-		}
-
-		void wait_for (task * t) {
-			auto& lane = this->get_thread_local_lane();
-
-			while (!t->is_complete())
-				lane.run_lane();
-		}
-
-		void run() {
-			if (_lanes[0].running)
-				return;
-
-			// setup workers
-			_lanes[0].id = std::this_thread::get_id();
-			_lanes[0].exec = this;
-			_lanes[0].running = true;
-
-			for (int i = 1; i < _lanes.size(); ++i) {
-				_lanes[i].worker = std::thread(
-					[](executor * exec, task_lane * lane) {
-						lane->exec = exec;
-						lane->id = std::this_thread::get_id();
-						lane->running = true;
-
-						while (lane->running)
-							lane->run_lane();
-					},
-					this,
-					& _lanes[i]
-				);
-			}
-		}
-
-		void stop() {
-
-			for (auto& lane : _lanes) {
-				lane.running = false;
-
-				if (lane.worker.joinable())
-					lane.worker.join();
-
-				std::cout << "lane for thread: " << lane.id << " | pushed: " << lane.tasks_pushed << " | executed: " << lane.tasks_executed << std::endl;
-			}
-		}
-
-		task* steal() {
-			auto rand_index = std::chrono::high_resolution_clock::now().time_since_epoch().count() % (_lanes.size());
-			return _lanes[rand_index].steal();
-		}
-
-		task_lane & get_thread_local_lane() {
-			auto id = std::this_thread::get_id();
-
-			for (auto & lane : _lanes) {
-				if (lane.id == id)
-					return lane;
-			}
-
-			return _lanes[0];
-		}
-
-		template < typename _callback_t, typename _t >
-		inline void parallel_for (_callback_t callback, _t * data, std::size_t length) {
-			
-			auto * t = get_thread_local_lane().local_allocator.alloc();
-			push_stream(callback, data, length, t);
-			
-			wait_for (t);
-		}
-
-		std::size_t executions{ 0 };
-
-	private:
-#if defined (_WIN32)
-		long const cache_size{
-			[]() -> long {
-				long line_size = 0;
-				DWORD buffer_size = 0;
-
-				GetLogicalProcessorInformationEx(RelationCache, nullptr, &buffer_size);
-				if (GetLastError() != ERROR_INSUFFICIENT_BUFFER)
-					return -1; // TODO: perhaps log
-
-				auto * buffer = (SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX *)malloc(buffer_size);
-
-				GetLogicalProcessorInformationEx(RelationCache, buffer, &buffer_size);
-
-				for (int i = 0; i != buffer_size / sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX); ++i) {
-					if (buffer[i].Cache.Level == 1) {
-						line_size = buffer[i].Cache.LineSize;
-						break;
-					}
-				}
-
-				free(buffer);
-				return line_size;
-
-			}()
-		};
-#else
-		long const cache_size { sysconf (_SC_LEVEL1_DCACHE_LINESIZE) };
-#endif
-
-		inline static std::size_t gcd (std::size_t a, std::size_t b) noexcept {
-			//return a == 0 ? b : gcd (b % a, a);
-
-			for (;;) {
-				a %= b;
-				if (a == 0)
-					return b;
-				b %= a;
-				if (b == 0)
-					return a;
-			}
-		}
-
-		// Calculate workload and create tasks
-		template < typename _t >
-		void push_stream(callable_type callback, _t* begin, std::size_t len, task * parent) {
-/*
-			// divide per cache line multiple --------------------------------
-			auto constexpr type_size { sizeof(_t) };
-			auto const block_size { gcd (type_size, cache_size) * 4 };
-
-			auto blocks = len / block_size;
-			auto rem = len % block_size;
-
-			decltype (len) offset { 0 };
-
-			// -------------------------------------
-			// create tasks
-			// -------------------------------------
-			auto & lane = get_thread_local_lane ();
-			parent->dependencies = blocks + (rem > 0 ? 1 : 0);
-
-			while (offset < len) {
-				// add task
-				auto * t = lane.local_allocator.alloc (callback, parent);
-
-				t->data 		= begin + offset;
-				t->data_length 	= block_size;
-
-				offset 			+= block_size;
-
-				if (offset < len)
-					t->data_length = block_size;
-				else
-					t->data_length = rem;
-
-				lane.push(t);
-			}
-*/
-
-			// divide per worker --------------------------------
-
-			auto constexpr type_size { sizeof(_t) };
-			auto const block_size { gcd (type_size, cache_size) };
-
-			auto blocks = len / block_size;
-			auto overflow = len % block_size;
-
-			auto worker_count = _lanes.size();
-			auto blocks_per_thread = blocks / worker_count;
-
-			overflow += (blocks_per_thread % worker_count) * block_size;
-
-			// -------------------------------------
-			// create tasks
-			// -------------------------------------
-			auto & lane = get_thread_local_lane ();
-
-			if (blocks_per_thread > 0) {
-				parent->unfinished_tasks.store(worker_count, std::memory_order_relaxed);
-
-				auto * cursor = begin;
-
-				for (int i = 0; i < worker_count; ++i) {
-					auto items = blocks_per_thread * block_size;
-
-					if (overflow >= cache_size) {
-						items += cache_size;
-						overflow -= cache_size;
-					} else if (i == worker_count - 1) {
-						items += overflow;
-					}
-
-					auto cursor_end = cursor + items;
-
-					// add task
-					auto * t = lane.local_allocator.alloc (callback, parent);
-					t->data = cursor;
-					t->data_length = items;
-
-					lane.push(t);
-
-					cursor = cursor_end;
-				}
-			} else {
-				parent->unfinished_tasks.store(1, std::memory_order_relaxed);
-				// add task
-				auto * t	= lane.local_allocator.alloc (callback, parent);
-				t->data	= begin;
-				t->data_length = len;
-
-				lane.push (t);
-			}
-
-			// linear
-
-			/*
-
-			auto& lane = get_thread_local_lane();
-
-			auto len = end - begin;
-			auto lane_count = _lanes.size();
-			auto task_size = len / lane_count;
-
-			parent->dependencies = lane_count;
-
-			for (int i = 0; i < lane_count; ++i) {
-				auto* t = lane.local_allocator.alloc(callback, parent);
-				t->begin = begin;
-				begin += task_size;
-				t->end = begin;
-
-				lane.push(t);
-			}
-
-			*/
-		}
-
-		std::vector < task_lane > 
-							_lanes;
-
-	};
-
-	void task_lane::run_lane() {
-		task* t = pop();
-
-		if (!t)
-			t = exec->steal();
-
-		if (t) {
-			t->run();
-			++tasks_executed;
-		}
-
-		std::this_thread::yield();
-	}
-
-}
-
-namespace proto_b {
-
-	using task_callback = void (*)(transformer*, std::size_t);
-
-	struct task {
+	struct alignas (32) task {
 	public:
 		
 		task_callback	callback;
@@ -530,22 +91,21 @@ namespace proto_b {
 
 		std::atomic_size_t
 						unresolved_children;
-
-		std::uint8_t	_padding_ [24];
 	};
 
-	struct lane {
+	struct alignas (32) lane {
 	public:
 
 		void push(task* t) noexcept {
-			auto b = back.load(std::memory_order_acquire);
+			auto b = back.load (std::memory_order_acquire);
 			tasks[b & mask] = t;
-			back.store(b + 1, std::memory_order_release);
+
+			back.store (b + 1, std::memory_order_release);
 		}
 
 		task* pop() noexcept {
-			auto b = back.fetch_sub(1) - 1;
-			auto f = front.load();
+			auto b = --back;
+			auto f = front.load(std::memory_order_acquire);
 
 			if (f <= b) {
 				task* t = tasks[b & mask];
@@ -554,26 +114,28 @@ namespace proto_b {
 					return t;
 
 				auto f_plus{ f + 1 };
-				if (!front.compare_exchange_strong(f, f_plus))
+				if (!front.compare_exchange_strong(f, f_plus, std::memory_order_release, std::memory_order_acquire))
 					t = nullptr;
 
-				back.store(f_plus);
+				back.store(f_plus, std::memory_order_release);
 				return t;
 			}
 			else {
-				back.store(f);
+				back.store(f, std::memory_order_release);
 				return nullptr;
 			}
 		}
 
 		task* steal() noexcept {
-			auto f = front.load();
-			auto b = back.load();
+			auto f = front.load(std::memory_order_relaxed);
+			auto b = back.load(std::memory_order_acquire);
+
+			std::atomic_thread_fence(std::memory_order_acq_rel);
 
 			if (f < b) {
 				auto* t = tasks[f & mask];
 
-				if (front.compare_exchange_strong(f, f + 1))
+				if (front.compare_exchange_strong(f, f + 1, std::memory_order_release))
 					return t;
 			}
 
@@ -602,8 +164,6 @@ namespace proto_b {
 
 		std::size_t task_buffer_index{ 0 };
 
-		uint8_t _padding_ [16];
-
 		static constexpr std::size_t mask{ 4096 - 1 };
 	};
 
@@ -617,8 +177,15 @@ namespace proto_b {
 		{}
 
 		~executor() {
-			for (auto& lane : lanes)
+			for (auto& lane : lanes) {
 				lane.running = false;
+			}
+
+			for (int i = 1; i < thread_count; ++i) {
+				if (workers [i].joinable())
+					workers[i].join();
+			}
+
 		}
 
 		inline static void run_lane(std::vector < lane > & lanes, lane & l, unsigned i) {
@@ -632,13 +199,15 @@ namespace proto_b {
 			}
 
 			if (t) {
-				t->callback(t->data, t->length);
+				exec_context cxt {l, t};
+				t->callback(cxt, t->data, t->length);
 
-				if (t->parent)
-					--(t->parent->unresolved_children);
+				if (t->parent) {
+					t->parent->unresolved_children.fetch_sub(1);
+				}
+			} else {
+				std::this_thread::yield();
 			}
-
-			std::this_thread::yield();
 		}
 
 		void run() {
@@ -661,48 +230,86 @@ namespace proto_b {
 		}
 
 		void wait_for(task* t) {
-			while (t->unresolved_children > 0) {
-				run_lane(lanes, lanes[0], 0);
+			auto & lane = get_this_lane();
+
+			while (t->unresolved_children.load() > 0) {
+				run_lane(lanes, lane, 0);
 			}	
+		}
+
+		void static divide_and_conquer (exec_context & cxt, void * data, std::size_t length) {
+			constexpr uint32_t slice_size {4096};
+
+			auto * t_data = reinterpret_cast<transformer *> (data);
+
+			auto * ta = cxt.lane.alloc ();
+			auto * tb = cxt.lane.alloc ();
+
+			ta->parent = tb->parent = cxt.task->parent;
+
+			if (length > slice_size * 2) {
+				ta->callback = tb->callback = &divide_and_conquer;
+			} else {
+				ta->callback = tb->callback = cxt.task->parent->callback;
+			}
+
+			ta->data = t_data;
+			ta->length = length / 2;
+
+			tb->data = t_data + ta->length;
+			tb->length = length - ta->length;
+
+			cxt.task->parent->unresolved_children.fetch_add(2);
+
+			cxt.lane.push(ta);
+			cxt.lane.push(tb);
 		}
 
 		void run_parallel(task_callback callback, transformer* data, std::size_t length) {
 
-			auto& lane = lanes[0];
+			auto & lane = get_this_lane();
 
-			task* parent = lane.alloc();
-			parent->unresolved_children = thread_count;
+			auto * parent = lane.alloc();
+			parent->callback = callback;
+			parent->unresolved_children.store (1);
 
-			auto stride = length / thread_count;
-			auto rem = length % thread_count;
+			parent->data = data;
+			parent->length = length;
+			parent->parent = nullptr;
 
-			for (unsigned i = 0; i < thread_count; ++i) {
-				auto offset = i * stride;
+			auto * t = lane.alloc ();
 
-				if (i == thread_count - 1)
-					stride = stride + rem;
+			t->data 	= data;
+			t->length 	= length;
+			t->parent 	= parent;
 
-				task* t = lane.alloc();
-				
-				t->callback = callback;
-				t->data = data + offset;
-				t->length = stride;
-				t->parent = parent;
+			t->callback = &divide_and_conquer;
 
-				lane.push(t);
-			}
+			lane.push (t);
 
-			wait_for(parent);
+			wait_for (parent);
+
 			lane.alloc_free();
 		}
 
-		std::vector < lane >	lanes;
+		inline lane & get_this_lane () noexcept {
+			auto id = std::this_thread::get_id();
+
+			for (int i = 1; i < workers.size(); ++i) {
+				if (workers[i].get_id() == id)
+					return lanes [i];
+			}
+
+			return lanes [0];
+		}
+
+		std::vector < lane >		lanes;
 		std::vector < std::thread >	workers;
 		unsigned const			thread_count;
 	};
 
 }
-
+/*
 void SequentialTransforms (benchmark::State& state) {
 	for (auto _ : state) {
 		auto range = state.range(0);
@@ -710,13 +317,15 @@ void SequentialTransforms (benchmark::State& state) {
 		for (int64_t i = 0; i < range; ++i)
 			test_data[i].update_matrix();
 	}
-}
+}*/
 
 #define THREAD_COUNT 4
 
 void CPPAsyncTransforms(benchmark::State& state) {
 
 	std::vector < std::future < void > > sync(THREAD_COUNT);
+
+	auto * data = test_data.data();
 
 	for (auto _ : state) {
 
@@ -732,7 +341,7 @@ void CPPAsyncTransforms(benchmark::State& state) {
 
 			sync[i] = std::async(std::launch::async, [=]() {
 				for (unsigned j = offset; j < stride; ++j)
-					test_data[j].update_matrix();
+					data[j].update_matrix();
 				});
 		}
 
@@ -741,8 +350,8 @@ void CPPAsyncTransforms(benchmark::State& state) {
 	}
 }
 
-proto_a::executor exe_a (THREAD_COUNT - 1);
-proto_b::executor exe_b(THREAD_COUNT);
+//proto_a::executor exe_a (THREAD_COUNT - 1);
+proto_c::executor exe_c(2);
 
 void parallel_for_runner (void * begin, std::size_t length) {
 	auto * it = static_cast < transformer * > (begin);
@@ -750,15 +359,12 @@ void parallel_for_runner (void * begin, std::size_t length) {
 	for (std::size_t i = 0; i < length; ++i)
 		it[i].update_matrix();
 }
-
+/*
 void ParallelTransforms_A(benchmark::State& state) {
 
 	exe_a.run();
 
 	for (auto _ : state) {
-
-		++exe_a.executions;
-
 		auto range = state.range(0);
 		exe_a.clear_alloc();
 
@@ -766,19 +372,21 @@ void ParallelTransforms_A(benchmark::State& state) {
 			parallel_for_runner,
 			test_data.data (), range);
 	}
-}
+}*/
 
-void ParallelTransforms_B(benchmark::State& state) {
+void ParallelTransforms_C(benchmark::State& state) {
 
-	exe_b.run();
+	exe_c.run();
 
 	for (auto _ : state) {
 
 		auto range = state.range(0);
 
-		exe_b.run_parallel([](transformer* data, std::size_t length) {
+		exe_c.run_parallel([](proto_c::exec_context &, void * data, std::size_t length) {
+				auto * t_data = reinterpret_cast < transformer * > (data);
+
 				for (std::size_t i = 0; i < length; ++i)
-					data[i].update_matrix();
+					t_data[i].update_matrix();
 			},
 			test_data.data(),
 			range);
@@ -786,17 +394,19 @@ void ParallelTransforms_B(benchmark::State& state) {
 }
 
 #define RANGE Range(MIN_ITERATION_RANGE, MAX_ITERATION_RANGE)
+
 /*
 BENCHMARK(SequentialTransforms)
 	->RANGE->Unit(benchmark::TimeUnit::kMillisecond);
-
+*/
 BENCHMARK(CPPAsyncTransforms)
 	->RANGE->Unit(benchmark::TimeUnit::kMillisecond);
 
+BENCHMARK(ParallelTransforms_C)
+	->RANGE->Unit(benchmark::TimeUnit::kMillisecond);
+/*
 BENCHMARK(ParallelTransforms_A)
-	->RANGE->Unit(benchmark::TimeUnit::kMillisecond);
-	*/
-BENCHMARK(ParallelTransforms_B)
-	->RANGE->Unit(benchmark::TimeUnit::kMillisecond);
+->RANGE->Unit(benchmark::TimeUnit::kMillisecond);
+*/
 
 BENCHMARK_MAIN();
