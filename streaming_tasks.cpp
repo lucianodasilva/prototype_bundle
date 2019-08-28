304077b7f96dd5663d207cc57d27eadecb58ae39
Luciano da Silva <luciano.silva@live.com.pt>
2019-08-28 19:42:18 +0100

    i don't really know what am i doing

--- streaming_tasks.cpp
+++ streaming_tasks.cpp
@@ -14,7 +14,7 @@
 #include <functional>
 #include <memory>
 #include <thread>
-
+#include <new>
 
 #if defined (_WIN32)
 #include <Windows.h>
@@ -25,10 +25,30 @@
 
 #include "static_ring_buffer.hpp"
 
-#define MIN_ITERATION_RANGE 1 << 14
-#define MAX_ITERATION_RANGE 1 << 20
+#if defined (__GNUC__) || defined (__clang__)
+inline uint8_t count_tzb (std::size_t v) noexcept {
+#	if defined (__x86_64__)
+	return static_cast < uint8_t > (__builtin_ctzl(v));
+#	else
+	return static_cast < uint8_t > (__builtin_ctz(v));
+#	endif
+}
+#elif defined (_MSC_VER)
+inline uint8_t count_tzb(std::size_t v) noexcept {
+				unsigned long c{ 0 };
+#if defined (_M_X64)
+				_BitScanForward64(&c, v);
+#else
+				_BitScanForward(&c, v);
+#endif
+				return static_cast < uint8_t > (c);
+			}
+#endif
 
-#define THREAD_COUNT std::thread::hardware_concurrency()
+#define MIN_ITERATION_RANGE 1 << 14
+#define MAX_ITERATION_RANGE 1 << 22
+
+#define THREAD_COUNT 8
 
 using namespace glm;
 
@@ -54,259 +74,13 @@ std::vector < transformer > generate_data(std::size_t count) {
 	return data;
 }
 
-auto test_data{ generate_data(MAX_ITERATION_RANGE) };
-
-// prototype implementation
-
-namespace proto_c {
-
-	struct task;
-	struct lane;
-
-	struct exec_context {
-		proto_c::lane & lane;
-		proto_c::task * task;
-	};
-
-	using task_callback = void (*)(exec_context & cxt, void *, std::size_t );
-
-	struct alignas (32) task {
-	public:
-		
-		task_callback	callback;
-		task const *	parent;
-		transformer*	data;
-		std::size_t		length;
-
-		mutable std::atomic_size_t
-						unresolved_children;
-	};
-
-	struct alignas (32) lane {
-	public:
-
-		void push(task* t) noexcept {
-			auto b = back.load (std::memory_order_acquire);
-			tasks[b & mask] = t;
-
-			back.store (b + 1, std::memory_order_release);
-		}
-
-		task* pop() noexcept {
-			auto b = --back;
-			auto f = front.load(std::memory_order_acquire);
-
-			if (f <= b) {
-				task* t = tasks[b & mask];
-
-				if (f != b)
-					return t;
-
-				auto f_plus{ f + 1 };
-				if (!front.compare_exchange_strong(f, f_plus, std::memory_order_release, std::memory_order_acquire))
-					t = nullptr;
-
-				back.store(f_plus, std::memory_order_release);
-				return t;
-			}
-			else {
-				back.store(f, std::memory_order_release);
-				return nullptr;
-			}
-		}
-
-		task* steal() noexcept {
-			auto f = front.load(std::memory_order_relaxed);
-			auto b = back.load(std::memory_order_acquire);
-
-			std::atomic_thread_fence(std::memory_order_acq_rel);
-
-			if (f < b) {
-				auto* t = tasks[f & mask];
-
-				if (front.compare_exchange_strong(f, f + 1, std::memory_order_release))
-					return t;
-			}
-
-			return nullptr;
-		}
-
-		inline task* alloc() noexcept {
-			++task_buffer_index;
-			return task_buffer.get() + task_buffer_index;
-		}
-
-		inline void alloc_free() noexcept {
-			task_buffer_index = 0;
-		}
-
-		std::atomic_int64_t
-			front{ 0 },
-			back { 0 };
-
-		// off critical path ( good ? / bad ? )
-		std::unique_ptr < task* [] > tasks { new task * [4096] };
-		std::unique_ptr < task [] >	 task_buffer { new task [4096] };
-
-		std::size_t task_buffer_index{ 0 };
-
-		bool
-			running{ false };
-
-		static constexpr std::size_t mask{ 4096 - 1 };
-	};
-
-	struct executor {
-	public:
-
-		executor(unsigned t_count) :
-			lanes(t_count),
-			workers (t_count),
-			thread_count (t_count)
-		{}
-
-		~executor() {
-			stop ();
-		}
-
-		inline void stop () {
-			for (auto& lane : lanes) {
-				lane.running = false;
-			}
-
-			for (int i = 1; i < thread_count; ++i) {
-				if (workers [i].joinable())
-					workers[i].join();
-			}
-		}
-
-		inline static void run_lane(std::vector < lane > & lanes, lane & l, unsigned i) {
-			auto* t = l.pop();
-
-			if (!t) {
-				auto rand_index = std::chrono::high_resolution_clock::now().time_since_epoch().count() % (lanes.size());
-
-				if (rand_index != i)
-					t = lanes[rand_index].steal();
-			}
-
-			if (t) {
-				exec_context cxt {l, t};
-				t->callback(cxt, t->data, t->length);
-
-				if (t->parent) {
-					t->parent->unresolved_children.fetch_sub(1);
-				}
-			} else {
-				std::this_thread::yield();
-			}
-		}
-
-		void run() {
-			if (lanes[0].running)
-				return;
-
-			lanes[0].running = true;
-
-			// init workers 
-			for (unsigned i = 1; i < thread_count; ++i) {
-				workers [i] = std::thread ([this, i]() {
-					auto & lane = lanes[i];
-
-					lane.running = true;
-
-					while (lane.running)
-						run_lane(lanes, lane, i);
-				});
-			}
-		}
-
-		void wait_for(task* t) {
-			auto & lane = get_this_lane();
-
-			while (t->unresolved_children.load() > 0) {
-				run_lane(lanes, lane, 0);
-			}	
-		}
-
-		void static divide_and_conquer (exec_context & cxt, void * data, std::size_t length) {
-			constexpr std::size_t slice_length { 1024 };
-			auto * t_data = reinterpret_cast<transformer *> (data);
-
-			auto * ta = cxt.lane.alloc ();
-			auto * tb = cxt.lane.alloc ();
-
-			ta->parent = tb->parent = cxt.task->parent;
-
-			if (length > (cxt.task->parent->length / 48)) {
-				ta->callback = tb->callback = &divide_and_conquer;
-			} else {
-				ta->callback = tb->callback = cxt.task->parent->callback;
-			}
-
-			ta->data = t_data;
-			ta->length = length / 2;
-
-			tb->data = t_data + ta->length;
-			tb->length = length - ta->length;
-
-			cxt.task->parent->unresolved_children.fetch_add(2);
-
-			cxt.lane.push(ta);
-			cxt.lane.push(tb);
-		}
-
-		void run_parallel(task_callback callback, transformer* data, std::size_t length) {
-
-			auto & lane = get_this_lane();
-
-			auto * parent = lane.alloc();
-			parent->callback = callback;
-			parent->unresolved_children.store (1);
-
-			parent->data = data;
-			parent->length = length;
-			parent->parent = nullptr;
-
-			auto * t = lane.alloc ();
-
-			t->data 	= data;
-			t->length 	= length;
-			t->parent 	= parent;
-
-			t->callback = &divide_and_conquer;
-
-			lane.push (t);
-
-			wait_for (parent);
-
-			for (auto & flane : lanes)
-				flane.alloc_free();
-		}
-
-		inline lane & get_this_lane () noexcept {
-			auto id = std::this_thread::get_id();
-
-			for (int i = 1; i < workers.size(); ++i) {
-				if (workers[i].get_id() == id)
-					return lanes [i];
-			}
-
-			return lanes [0];
-		}
-
-		std::vector < lane >		lanes;
-		std::vector < std::thread >	workers;
-		unsigned const			thread_count;
-	};
-
-}
+std::vector < transformer > test_data( MAX_ITERATION_RANGE);
 
 namespace proto_d {
 
 	using task_callback = std::function < void () >;
 
-	struct alignas (std::hardware_constructive_interference_size) task {
+	struct alignas (64) task {
 	public:
 
 		task_callback	callback;
@@ -316,7 +90,7 @@ namespace proto_d {
 						unresolved_children;
 	};
 
-	struct alignas (std::hardware_constructive_interference_size) lane {
+	struct alignas (64) lane {
 	public:
 
 		void push(task* t) noexcept {
@@ -378,15 +152,15 @@ namespace proto_d {
 			back { 0 };
 
 		// off critical path ( good ? / bad ? )
-		std::unique_ptr < task * [] >	tasks		{ new task * [4096] };
-		std::unique_ptr < task [] >		task_buffer	{ new task [4096] };
+		std::unique_ptr < task * [] >	tasks		{ new task * [8192] };
+		std::unique_ptr < task [] >		task_buffer	{ new task [8192] };
 
 		std::size_t task_buffer_index{ 0 };
 
 		bool
 			running{ false };
 
-		static constexpr std::size_t mask{ 4096 - 1 };
+		static constexpr std::size_t mask{ 8192 - 1 };
 	};
 
 	struct executor {
@@ -425,10 +199,6 @@ namespace proto_d {
 
 			if (t) {
 				t->callback();
-
-				if (t->parent) {
-					t->parent->unresolved_children.fetch_sub(1);
-				}
 			} else {
 				std::this_thread::yield();
 			}
@@ -465,22 +235,28 @@ namespace proto_d {
 		inline void run_parallel(_callback_t callback, _data_t * data, std::size_t length) {
 			auto& lane = get_this_lane();
 
+			//unsigned job_div = std::sqrt(length);
+			unsigned job_div = THREAD_COUNT;
+
 			task* parent = lane.alloc();
-			parent->unresolved_children = thread_count;
+			parent->unresolved_children = job_div;
 
-			auto stride = length / thread_count;
-			auto rem = length % thread_count;
+			auto stride = length / job_div;
+			auto rem = length % job_div;
 
-			for (unsigned i = 0; i < thread_count; ++i) {
+			for (unsigned i = 0; i < job_div; ++i) {
 				auto offset = i * stride;
 
-				if (i == thread_count - 1)
+				if (i == job_div - 1)
 					stride = stride + rem;
 
 				task* t = lane.alloc();
 
-				t->callback = [=]() { 
-					callback(data + offset, stride);
+				t->callback = [task_data { data + offset }, stride, callback, parent]() {
+					for (unsigned j = 0; j < stride; ++j)
+						callback(task_data [j]);
+
+					--(parent->unresolved_children);
 				};
 
 				t->parent = parent;
@@ -514,8 +290,6 @@ namespace proto_d {
 
 void BASELINE(benchmark::State& state) {
 
-/*
-
 	std::vector < std::future < void > > sync(THREAD_COUNT);
 
 	auto * data = test_data.data();
@@ -533,7 +307,7 @@ void BASELINE(benchmark::State& state) {
 				stride = stride + rem;
 
 			sync[i] = std::async(std::launch::async, [=]() {
-				for (unsigned j = offset; j < stride; ++j)
+				for (unsigned j = offset; j < offset + stride; ++j)
 					data[j].update_matrix();
 				});
 		}
@@ -541,7 +315,8 @@ void BASELINE(benchmark::State& state) {
 		for (unsigned i = 0; i < THREAD_COUNT; ++i)
 			sync[i].get();
 	}
- */
+
+/*
 	for (auto _ : state) {
 		std::for_each (
 			std::execution::par,
@@ -551,32 +326,19 @@ void BASELINE(benchmark::State& state) {
 				v.update_matrix();
 			}
 		);
-	}
+	}*/
 }
 
-//proto_a::executor exe_a (THREAD_COUNT - 1);
-proto_c::executor exe_c(THREAD_COUNT);
 proto_d::executor exe_d(THREAD_COUNT);
 
-void PROTO_C(benchmark::State& state) {
-
-	exe_c.run();
-
+void SEQ_BASELINE(benchmark::State & state) {
 	for (auto _ : state) {
 
 		auto range = state.range(0);
 
-		exe_c.run_parallel([](proto_c::exec_context &, void * data, std::size_t length) {
-				auto * t_data = reinterpret_cast < transformer * > (data);
-
-				for (std::size_t i = 0; i < length; ++i)
-					t_data[i].update_matrix();
-			},
-			test_data.data(),
-			range);
+		for (unsigned i = 0; i < range; ++i)
+			test_data [i].update_matrix();
 	}
-
-	exe_c.stop();
 }
 
 void PROTO_D(benchmark::State& state) {
@@ -587,9 +349,8 @@ void PROTO_D(benchmark::State& state) {
 
 		auto range = state.range(0);
 
-		exe_d.run_parallel([](auto * data, std::size_t length) {
-							   for (std::size_t i = 0; i < length; ++i)
-								   data[i].update_matrix();
+		exe_d.run_parallel([](auto & item) {
+							   item.update_matrix();
 						   },
 						   test_data.data(),
 						   range);
@@ -598,12 +359,12 @@ void PROTO_D(benchmark::State& state) {
 
 #define RANGE Range(MIN_ITERATION_RANGE, MAX_ITERATION_RANGE)
 
-/*
-BENCHMARK(SequentialTransforms)
+
+BENCHMARK(SEQ_BASELINE)
 	->RANGE->Unit(benchmark::TimeUnit::kMillisecond);
-*/
-//BENCHMARK(BASELINE)
-//	->RANGE->Unit(benchmark::TimeUnit::kMillisecond);
+
+BENCHMARK(BASELINE)
+	->RANGE->Unit(benchmark::TimeUnit::kMillisecond);
 
 //BENCHMARK(PROTO_C)
 //->RANGE->Unit(benchmark::TimeUnit::kMillisecond);
